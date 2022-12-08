a7f7199089d1f880b544c189b786ab78a5e2d491
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2022-12-08 16:42:45 -0300

    Update 161.c

--- ex1.6.1/161.c
+++ ex1.6.1/161.c
@@ -28,20 +28,21 @@ int main (int argc, char* args[])
     SDL_Rect r3 = { 40,400, 40,40 };
     int espera = 500;
     bool continua = true;
-    bool fimdejogo = false;
     bool selecionado = false;
     bool arrastado = false;
     bool cancelado = false;
-    int x, y, dx, dy,xAntes,yAntes,xDepois,yDepois;
+    int mensagem = false;
+    int x, y, dx, dy,xAntes,yAntes,xDepois,yDepois = 0;
+    
      /* EXECUÇÃO */
     while (continua) {
-	SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+	SDL_SetRenderDrawColor(ren, 0x00,0x00,0x00,0x00);
 	SDL_RenderClear(ren);
 	SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00);
 	SDL_RenderFillRect(ren, &r3);
-	if(!selecionado && r3.x == xAntes && r3.x !=xDepois && !cancelado ) stringRGBA(ren, 50, 50, "Clicked...", 0, 255, 0, 255);
-	else if(!selecionado && arrastado && !cancelado) stringRGBA(ren, 50, 50, "arrastado", 0, 255, 0, 255);
-	else if(cancelado) stringRGBA(ren, 50, 50, "cancelado", 0, 255, 0, 255);
+	if(!selecionado && r3.x == xAntes && r3.x !=xDepois && !cancelado && r3.y == yAntes && r3.y != yDepois) stringRGBA(ren, 50, 50, "Clicado...", 0, 255, 0, 255);
+	else if(!selecionado && arrastado && !cancelado &&  xAntes != xDepois) stringRGBA(ren, 50, 50, "Arrastado..", 0, 255, 0, 255);
+	
 	SDL_RenderPresent(ren);
 	SDL_Event evt;       
 	if (AUX_WaitEventTimeoutCount(&evt,&espera)) {		 
@@ -49,42 +50,58 @@ int main (int argc, char* args[])
 			case SDL_KEYDOWN:	   	
 				switch (evt.key.keysym.sym){  
 				    case SDLK_ESCAPE:
-				    	if(selecionado){ 
+				    	if(selecionado && r3.x != xAntes && r3.y != yAntes ){ 
 						r3.x = xAntes;
 						r3.y = yAntes;	
-						cancelado = true;											
+						selecionado = false;
+						arrastado = false;
+						cancelado = true;
 					}
-				}
-			 		
+				     break;	
+				}break;
+			 	
 			case SDL_MOUSEBUTTONDOWN:
-				SDL_GetMouseState(&x,&y);
-				if(x > r3.x && x < r3.x + r3.w && y > r3.y && y < r3.y + r3.h){
-					selecionado = true;
-					cancelado = false;
-				    	dx=r3.x-x;
-					dy=r3.y-y;
-					xAntes = r3.x;
-					yAntes = r3.y;
-				 } 
+				if(evt.button.button==SDL_BUTTON_LEFT){
+					SDL_GetMouseState(&x,&y);
+					if(x > r3.x && x < r3.x + r3.w && y > r3.y && y < r3.y + r3.h){
+						selecionado = true;
+						cancelado = false;
+					    	dx=r3.x-x;
+						dy=r3.y-y;
+						xAntes = r3.x;
+						yAntes = r3.y;
+					 }
+					 else{
+					 	selecionado = false;
+						cancelado = true;
+					 } 
+					 break;
+				}
 			case SDL_MOUSEBUTTONUP:
-				if(evt.button.state==SDL_RELEASED){
-					 selecionado=false;
-					 arrastado = true;;
-					 SDL_GetMouseState(&x,&y);
-					 xDepois = x;
-					 yDepois = y;
-					
-					 
+				if(evt.button.button==SDL_BUTTON_LEFT){
+					SDL_GetMouseState(&x,&y);
+					if(evt.button.state==SDL_RELEASED){
+						selecionado=false;
+						if(x > r3.x && x < r3.x + r3.w && y > r3.y && y < r3.y + r3.h){
+							 arrastado = true;
+							 xDepois = x;
+							 yDepois = y;
+						 }
+					}
+					break;
 				}
 			case SDL_MOUSEMOTION:
 				SDL_GetMouseState(&x,&y);
-				if(x <= 460 && y <= 460 && selecionado){
+				if(r3.x <= 460 && r3.y <= 460 && selecionado){
 					r3.x=x+dx;
 					r3.y = y+dy;
 					arrastado = false;
-				}	
+				}
+			break;
+
 		}
-	} 
+	}
+	 
 	
 	switch (evt.type ) {
 	    case SDL_QUIT:
